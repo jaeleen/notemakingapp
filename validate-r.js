02d0f760f4335b1f7140fe31cdd863f3f06844cc
jaeleen <jaeleen.pg@gmail.com>
2022-02-20 22:38:25 -0600

    Update signup.html

--- validate-r.js
+++ validate-r.js
@@ -1,3 +1,5 @@
 // Event registers for Login.html
 
 document.getElementById("LogIn").addEventListener("submit", LogInForm, false);
+document.getElementById("SignUp").addEventListener("submit", SignUpForm, false);
+
--- validate.js
+++ validate.js
@@ -14,7 +14,6 @@ function LogInForm(event){
     var regex_email = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     var regex_uname = /^[a-zA-Z0-9_-]+$/;
     var regex_pswd  = /^(\S*)?\d+(\S*)?$/;
-    var regex_login_pswd = /^/;
 
     var email_msg = document.getElementById("email_msg");
     var pswd_msg = document.getElementById("pswd_msg");
@@ -55,12 +54,71 @@ function LogInForm(event){
     // Add code for if validation is true.
 }
 
-function CreateNoteForm(event){
+function SignUpForm(event){
     event.preventDefault();
 
 
     var valid = true;
+    // var errormessage = "";
+    // var user_inputs = "";
+
+
+    var elements = event.currentTarget;
+    var email = elements[0].value; 
+    var username = elements[1].value; 
+    var password = elements[2].value;
+    var passwordrepeat = elements[3].value;
+
+
+    var regex_email = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var regex_uname = /^[a-zA-Z0-9_-]+$/;
+    var regex_pswd  = /^(\S*)?\d+(\S*)?$/;
+
+    var email_msg = document.getElementById("signup_email_msg");
+    var uname_msg = document.getElementById("signup_uname_msg");
+    var pswd_msg = document.getElementById("signup_pswd_msg");
+    var pswdr_msg = document.getElementById("signup_pswdr_msg");
+
+    email_msg.innerHTML  = "";
+    uname_msg.innerHTML = "";
+    pswd_msg.innerHTML = "";
+    pswdr_msg.innerHTML = "";
+
+    //Variables for DOM Manipulation commands
+    var textNode;
+    var htmlNode;
+
+    // Validate Username
+    if(email == null || email == ""){
+        textNode = document.createTextNode("Email address is empty.");
+        email_msg.appendChild(textNode);
+        valid = false;
+    }
+    else if(regex_email.test(email) == false){
+        textNode = document.createTextNode("Follow email format: someone@gmail.com");
+        email_msg.appendChild(textNode);
+        valid = false;
+    }
+
+    if(password ==null || password ==""){
+        textNode = document.createTextNode("Password is empty.");
+        pswd_msg.appendChild(textNode);
+        valid = false;
+    }
+    else if(password.length <= 6){
+        textNode = document.createTextNode("Password has to be 6 characters or more. \n");
+        pswd_msg.appendChild(textNode);
+        valid = false;
+    }
+
+    else{
+        alert("Signup successful!");
+        event.preventDefault(); //remove for final submission
+    }
+    // Add code for if validation is true.
 }
+
+
 
 
 
