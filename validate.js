58b2723e01cef3a8c14750e6c24f83b273681d33
jaeleen <jaeleen.pg@gmail.com>
2022-02-21 21:18:36 -0600

    Add Javascipt form validation and dynamic counter

--- validate.js
+++ validate.js
@@ -54,71 +54,29 @@ function LogInForm(event){
     // Add code for if validation is true.
 }
 
-function SignUpForm(event){
-    event.preventDefault();
 
+// Dynamic counter for Create Note Page
+function countChars(obj) {
+    var maxLength = 256;
+    var strLength = obj.value.length;
 
-    var valid = true;
-    // var errormessage = "";
-    // var user_inputs = "";
-
-
-    var elements = event.currentTarget;
-    var email = elements[0].value; 
-    var username = elements[1].value; 
-    var password = elements[2].value;
-    var passwordrepeat = elements[3].value;
-
-
-    var regex_email = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    var regex_uname = /^[a-zA-Z0-9_-]+$/;
-    var regex_pswd  = /^(\S*)?\d+(\S*)?$/;
-
-    var email_msg = document.getElementById("signup_email_msg");
-    var uname_msg = document.getElementById("signup_uname_msg");
-    var pswd_msg = document.getElementById("signup_pswd_msg");
-    var pswdr_msg = document.getElementById("signup_pswdr_msg");
-
-    email_msg.innerHTML  = "";
-    uname_msg.innerHTML = "";
-    pswd_msg.innerHTML = "";
-    pswdr_msg.innerHTML = "";
-
-    //Variables for DOM Manipulation commands
-    var textNode;
-    var htmlNode;
-
-    // Validate Username
-    if(email == null || email == ""){
-        textNode = document.createTextNode("Email address is empty.");
-        email_msg.appendChild(textNode);
-        valid = false;
+    if(strLength > maxLength){
+        document.getElementById("charNum").innerHTML = '<span style="color: red;">'+strLength+' / '+maxLength+' characters</span>';
+    }else{
+        document.getElementById("charNum").innerHTML = strLength+' / '+maxLength+' characters';
     }
-    else if(regex_email.test(email) == false){
-        textNode = document.createTextNode("Follow email format: someone@gmail.com");
-        email_msg.appendChild(textNode);
-        valid = false;
-    }
-
-    if(password ==null || password ==""){
-        textNode = document.createTextNode("Password is empty.");
-        pswd_msg.appendChild(textNode);
-        valid = false;
-    }
-    else if(password.length <= 6){
-        textNode = document.createTextNode("Password has to be 6 characters or more. \n");
-        pswd_msg.appendChild(textNode);
-        valid = false;
-    }
-
-    else{
-        alert("Signup successful!");
-        event.preventDefault(); //remove for final submission
-    }
-    // Add code for if validation is true.
 }
 
+// Dynamic counter for View / contribute Note Page
+function countNumOfChars(obj) {
+    var maxLength = 1500;
+    var strLength = obj.value.length;
 
-
+    if(strLength > maxLength){
+        document.getElementById("numofchars").innerHTML = '<span style="color: red;">'+strLength+' / '+maxLength+' characters</span>';
+    }else{
+        document.getElementById("numofchars").innerHTML = strLength+' / '+maxLength+' characters';
+    }
+}
 
 
